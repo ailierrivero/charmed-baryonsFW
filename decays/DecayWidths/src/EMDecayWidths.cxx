44d48915ce37288f47c935dbbc5187c9feca3285
Ailierrivero <ailierrivero@gmail.com>
2024-12-30 12:11:25 -0500

    Activated D wave

--- decays/DecayWidths/src/EMDecayWidths.cxx
+++ decays/DecayWidths/src/EMDecayWidths.cxx
@@ -28,7 +28,7 @@ double EMDecayWidths::execute(double ma_val, double sa_val, double ja_val, doubl
   MA = ma_val;
   MB = mb_val;  
   if(MA<MB) return 0.; //energy conservation
-  if(MA>3.100) return 0.;
+  // if(MA>3.100) return 0.; //to calculate only up to P-wave Xi_c
 
   // quark masses
   mcharm = mcharm_val;
